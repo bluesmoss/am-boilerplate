6b608d2a7feb161d5ac98af911f70376e062cc37
Alfredo Morales Sánchez <bluesmoss@gmail.com>
2021-07-24 18:29:01 -0500

    chore(routing): define main sections

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const port = 3000;
 
-// respond with "hello world" when a GET request is made to the homepage
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
 app.get('/', function (req, res) {
-  res.send('hello world')
+  res.render('pages/home')
+})
+
+app.get('/about', function (req, res) {
+  res.render('pages/about')
+})
+
+app.get('/collections', function (req, res) {
+  res.render('pages/collections')
+})
+
+app.get('/detail/:id', function (req, res) {
+  res.render('pages/detail')
 })
 
 app.listen(port, () => {
-    console.log(`Example app listeing at port: ${port}`);
+    console.log(`Example app listeing at port: http://localhost:${port}`);
 })

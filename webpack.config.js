aca65cdcd22514b94bd167053cc7d72357c4f260
Alfredo Morales Sánchez <bluesmoss@gmail.com>
2021-07-24 10:59:24 -0500

    build(webpack): add modules settings

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,10 @@ const path  = require('path');
 const webpack = require('webpack');
 
 const copyWebpackPlugin =  require('copy-webpack-plugin');
-const minissExtractPlugin = require('mini-css-extract-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
 const IS_DEVELOPMENT = process.env.NODE.ENV === 'dev';
 
@@ -45,6 +47,85 @@ module.exports = {
           new MiniCssExtractPlugin({
               filename: '[name].css',
               chunkFilename: '[id].css'
-          })
+          }),
+
+          new ImageMinimizerPlugin({
+            minimizerOptions: {
+              plugins: [
+                ['gifsicle', { interlaced: true }],
+                ['jpegtran', { progressive: true }],
+                ['optipng', { optimizationLevel: 8 }]
+              ]
+            }
+          }),
+      
+          new CleanWebpackPlugin()          
     ],
+
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: {
+            loader: 'babel-loader'
+          }
+        },
+  
+        {
+          test: /\.scss$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                publicPath: ''
+              }
+            },
+            {
+              loader: 'css-loader'
+            },
+            {
+              loader: 'postcss-loader'
+            },
+            {
+              loader: 'sass-loader'
+            }
+          ]
+        },
+  
+        {
+          test: /\.(jpe?g|png|gif|svg|woff2?|fnt|webp)$/,
+          loader: 'file-loader',
+          options: {
+            name (file) {
+              return '[hash].[ext]'
+            }
+          }
+        },
+  
+        {
+          test: /\.(jpe?g|png|gif|svg|webp)$/i,
+          use: [
+            {
+              loader: ImageMinimizerPlugin.loader
+            }
+          ]
+        },
+  
+        {
+          test: /\.(glsl|frag|vert)$/,
+          loader: 'raw-loader',
+          exclude: /node_modules/
+        },
+  
+        {
+          test: /\.(glsl|frag|vert)$/,
+          loader: 'glslify-loader',
+          exclude: /node_modules/
+        }
+      ]
+    },
+    optimization: {
+      minimize: true,
+      minimizer: [new TerserPlugin()]
+    }
 }
